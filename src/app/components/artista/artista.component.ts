d6ed33627bf98bda8e7e10e302d2c1bfa5ce2a9d
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-20 00:44:50 -0500

    first commit

---
+++ src/app/components/artista/artista.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artista',
+  templateUrl: './artista.component.html',
+})
+export class ArtistaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
